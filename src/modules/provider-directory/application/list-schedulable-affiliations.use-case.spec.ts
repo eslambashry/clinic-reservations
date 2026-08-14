a4ff3e48d11b399b3ba7c343692e8b873c114946
Yussef Makhlouf <yussef.ali.it@gmail.com>
2026-08-14 03:58:03 +0300

    fix: add missing import statement for testing utilities in ListSchedulableAffiliationsUseCase tests

--- src/modules/provider-directory/application/list-schedulable-affiliations.use-case.spec.ts
+++ src/modules/provider-directory/application/list-schedulable-affiliations.use-case.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect, jest } from '@jest/globals';
 import { ListSchedulableAffiliationsUseCase } from './list-schedulable-affiliations.use-case';
 
 function affiliation(overrides: Partial<any> = {}) {
